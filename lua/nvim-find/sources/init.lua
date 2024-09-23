0d8f2e0e5e15673fa39555f89707ef7b14577a4b
benceBakos <bakos.bence.work@gmail.com>
2024-09-23 20:50:08 +0200

    fix find not found

--- lua/nvim-find/sources/init.lua
+++ lua/nvim-find/sources/init.lua
@@ -3,6 +3,7 @@
 local sources = {}
 
 sources.buffers = require("nvim-find.sources.buffers").run
+sources.find = require("nvim-find.sources.find").run
 sources.fd = require("nvim-find.sources.fd").run
 sources.rg_grep = require("nvim-find.sources.rg").grep
 sources.rg_files = require("nvim-find.sources.rg").files
